1ad02b08cb3803020c56c391c8fa4e092447a0fb
Trazler McGrath <trazmcg@gmail.com>
2022-11-07 16:19:36 -0800

    Reduce redundancy in SqlWrapper

--- Project-2-NFL/qsqlfootball.h
+++ Project-2-NFL/qsqlfootball.h
@@ -22,37 +22,27 @@ public:
      * Story #2
      * Return all info for one team in a QWidget
      */
-    const QWidget getInfo(const QString TEAM_NAME) const;
+    const QWidget getTeamInfo(const QString TEAM_NAME) const;
     /** getAllTeams
      * Story #3
      * Teams are sorted alphabetically.
      */
-    const QWidget getAllTeams() const;
+    const QWidget getTeams(const QString SEARCH_TERM = "*",  const QString FILTER_TERM = "Team(s)", const QString SORT_TERM = "team_name") const;
     /** getAllStadiums
      * Story #4
      * Stadiums are sorted alphabetically. Each stadium's corresponding team(s) are stored in
      */
-    const QWidget getAllStadiumsSortBy(const QString TEAM_NAME, const QString SORT_TERM = "alphabetical") const;
-    /** getInfo
-     * Story #5
-     * Teams are sorted alphabetically.
-     */
-    const QWidget getTeamsFilterByConference(const QString CONFERENCE_NAME) const;
-    /** getInfo
-     * Story #7
-     * Teams are sorted alphabetically.
-     */
-    const QWidget getTeamsFilterByDivision(const QString TEAM_NAME) const;
-    /** getInfo
-     * Story #8
-     * Stadiums are sorted alphabetically. Each stadium's corresponding team(s) are stored in
-     */
-    const QWidget getStadiumsFilterByRoof(const QString STADIUM_ROOF_TYPE) const;
+    const QWidget getStadiums(const QString SEARCH_TERM = "*",  const QString FILTER_TERM = "Team(s)", const QString SORT_TERM = "team_name") const;
     /** getAllTeams
      * Story #9
      * Teams are sorted alphabetically. Each team's corresponding stars
      */
     const QWidget getAllTeamsStars() const;
+    /** getAllTeams
+     * Story #9
+     * Teams are sorted alphabetically. Each team's corresponding stars
+     */
+    const QWidget getAllTeamsSouveniers() const;
 
 };
 
