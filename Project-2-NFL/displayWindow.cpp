e38b7af71dfa6752ddc196ed0316e943a3f232d4
nedamhs <94092859+nedamhs@users.noreply.github.com>
2022-11-16 11:58:39 -0800

    displays

---
+++ Project-2-NFL/displayWindow.cpp
@@ -0,0 +1,133 @@
+#include "displayWindow.h"
+#include "ui_displayWindow.h"
+
+
+
+displayWindow::displayWindow(QWidget *parent)
+    : QWidget(parent), ui(new Ui::displayWindow)
+{
+    ui->setupUi(this);
+
+    QSqlDatabase myDb;
+
+    if(QSqlDatabase::contains("qt_sql_default_connection"))
+    {
+        myDb = QSqlDatabase::database("qt_sql_default_connection");
+    }
+    else
+    {
+        myDb = QSqlDatabase::addDatabase("QSQLITE");
+    }
+
+    myDb.setDatabaseName("../NFLProject.db");
+    if (myDb.open())
+    {
+        qDebug().noquote() << "db found and open";
+    }
+    else
+    {
+        qDebug().noquote() << "db not found";
+    }
+
+    QSqlQueryModel* qryModel = new QSqlQueryModel();
+    ui->tableView->setAlternatingRowColors(true);
+    qryModel->setQuery("SELECT * FROM Teams");
+    ui->tableView->setModel(qryModel);
+
+
+    // code for combobox options
+    QSqlQueryModel* comboquery = new QSqlQueryModel();
+    comboquery->setQuery("SELECT Team FROM Teams");
+    ui->comboBox->setModel(comboquery);
+
+}
+// =======================================================
+displayWindow::~displayWindow()
+{
+    delete ui;
+}
+// =======================================================
+
+void displayWindow :: on_pushButton_selectTeam_clicked()
+{
+    // stores the team selected in combobox in a variable
+    QString selectedTeam = ui-> comboBox ->currentText();
+
+    QSqlQueryModel* qryModel = new QSqlQueryModel();
+    ui->tableView->setAlternatingRowColors(true);
+    qryModel->setQuery("SELECT * FROM Teams WHERE Team LIKE '%"+selectedTeam +"%' ");
+    ui->tableView->setModel(qryModel);
+}
+// =======================================================
+void displayWindow :: on_pushButton_displayTeam_clicked()
+{
+    QSqlQueryModel* qryModel = new QSqlQueryModel();
+    ui->tableView->setAlternatingRowColors(true);
+    qryModel->setQuery("SELECT Team FROM Teams ORDER BY Team");
+    ui->tableView->setModel(qryModel);
+}
+// =======================================================
+void displayWindow :: on_pushButton_displayStadium_clicked()
+{
+    QSqlQueryModel* qryModel = new QSqlQueryModel();
+    ui->tableView->setAlternatingRowColors(true);
+    qryModel->setQuery("SELECT Name, Team FROM Teams ORDER BY Name");
+    ui->tableView->setModel(qryModel);
+}
+// =======================================================
+void displayWindow :: on_pushButton_AFC_clicked()
+{
+    QSqlQueryModel* qryModel = new QSqlQueryModel();
+    ui->tableView->setAlternatingRowColors(true);
+    qryModel->setQuery("SELECT Team, Conference FROM Teams WHERE Conference LIKE '%American Football Conference%' ORDER BY Team");
+    ui->tableView->setModel(qryModel);
+}
+// =======================================================
+void displayWindow :: on_pushButton_NFC_clicked()
+{
+    QSqlQueryModel* qryModel = new QSqlQueryModel();
+    ui->tableView->setAlternatingRowColors(true);
+    qryModel->setQuery("SELECT Team, Conference FROM Teams WHERE Conference LIKE '%National Football Conference%' ORDER BY Team");
+    ui->tableView->setModel(qryModel);
+}
+// =======================================================
+void displayWindow :: on_pushButton_NFCNORTH_clicked()
+{
+    QSqlQueryModel* qryModel = new QSqlQueryModel();
+    ui->tableView->setAlternatingRowColors(true);
+    qryModel->setQuery("SELECT Team, Division FROM Teams WHERE Division LIKE 'NFC%North' ORDER BY Team");
+    ui->tableView->setModel(qryModel);
+}
+// =======================================================
+void displayWindow :: on_pushButton_openRoof_clicked()
+{
+    QSqlQueryModel* qryModel = new QSqlQueryModel();
+    ui->tableView->setAlternatingRowColors(true);
+    qryModel->setQuery("SELECT Name,Team, RoofType FROM Teams WHERE RoofType LIKE '%open%' ORDER BY Name");
+    ui->tableView->setModel(qryModel);
+}
+// =======================================================
+void displayWindow :: on_pushButton_stadiumByDate_clicked()
+{
+    QSqlQueryModel* qryModel = new QSqlQueryModel();
+    ui->tableView->setAlternatingRowColors(true);
+    qryModel->setQuery("SELECT Name,Team, Opened FROM Teams ORDER BY Opened");
+    ui->tableView->setModel(qryModel);
+}
+// =======================================================
+void displayWindow :: on_pushButton_stadiumByCapacity_clicked()
+{
+    QSqlQueryModel* qryModel = new QSqlQueryModel();
+    ui->tableView->setAlternatingRowColors(true);
+    qryModel->setQuery("SELECT Name,Team, Capacity FROM Teams ORDER BY Capacity");
+    ui->tableView->setModel(qryModel);
+}
+// =======================================================
+void displayWindow :: on_pushButton_teamsByConf_clicked()
+{
+    QSqlQueryModel* qryModel = new QSqlQueryModel();
+    ui->tableView->setAlternatingRowColors(true);
+    qryModel->setQuery("SELECT Team, Name, Conference, Location FROM Teams ORDER BY Conference");
+    ui->tableView->setModel(qryModel);
+}
+// =======================================================
---
+++ build-Project-2-NFL-Qt_6_2_4_for_macOS-Debug/ui_admin.h
@@ -0,0 +1,99 @@
+/********************************************************************************
+** Form generated from reading UI file 'admin.ui'
+**
+** Created by: Qt User Interface Compiler version 6.2.4
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_ADMIN_H
+#define UI_ADMIN_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QComboBox>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QTableView>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_Admin
+{
+public:
+    QWidget *verticalLayoutWidget;
+    QVBoxLayout *verticalLayout;
+    QTableView *tableView;
+    QHBoxLayout *horizontalLayout;
+    QComboBox *comboBox;
+    QPushButton *addButton;
+    QPushButton *pushButton_3;
+    QPushButton *pushButton_2;
+
+    void setupUi(QWidget *Admin)
+    {
+        if (Admin->objectName().isEmpty())
+            Admin->setObjectName(QString::fromUtf8("Admin"));
+        Admin->resize(793, 464);
+        verticalLayoutWidget = new QWidget(Admin);
+        verticalLayoutWidget->setObjectName(QString::fromUtf8("verticalLayoutWidget"));
+        verticalLayoutWidget->setGeometry(QRect(30, 20, 741, 191));
+        verticalLayout = new QVBoxLayout(verticalLayoutWidget);
+        verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
+        verticalLayout->setContentsMargins(0, 0, 0, 0);
+        tableView = new QTableView(verticalLayoutWidget);
+        tableView->setObjectName(QString::fromUtf8("tableView"));
+
+        verticalLayout->addWidget(tableView);
+
+        horizontalLayout = new QHBoxLayout();
+        horizontalLayout->setObjectName(QString::fromUtf8("horizontalLayout"));
+        comboBox = new QComboBox(verticalLayoutWidget);
+        comboBox->setObjectName(QString::fromUtf8("comboBox"));
+
+        horizontalLayout->addWidget(comboBox);
+
+        addButton = new QPushButton(verticalLayoutWidget);
+        addButton->setObjectName(QString::fromUtf8("addButton"));
+
+        horizontalLayout->addWidget(addButton);
+
+        pushButton_3 = new QPushButton(verticalLayoutWidget);
+        pushButton_3->setObjectName(QString::fromUtf8("pushButton_3"));
+
+        horizontalLayout->addWidget(pushButton_3);
+
+        pushButton_2 = new QPushButton(verticalLayoutWidget);
+        pushButton_2->setObjectName(QString::fromUtf8("pushButton_2"));
+
+        horizontalLayout->addWidget(pushButton_2);
+
+
+        verticalLayout->addLayout(horizontalLayout);
+
+
+        retranslateUi(Admin);
+
+        QMetaObject::connectSlotsByName(Admin);
+    } // setupUi
+
+    void retranslateUi(QWidget *Admin)
+    {
+        Admin->setWindowTitle(QCoreApplication::translate("Admin", "Form", nullptr));
+        addButton->setText(QCoreApplication::translate("Admin", "Add", nullptr));
+        pushButton_3->setText(QCoreApplication::translate("Admin", "Edit", nullptr));
+        pushButton_2->setText(QCoreApplication::translate("Admin", "Delete", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class Admin: public Ui_Admin {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_ADMIN_H
